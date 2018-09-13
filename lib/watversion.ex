a8b266a13803a9a4425e70a88194ebe3acab6997
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2018-09-13 21:44:28 +0200

    Add .travis.yml

--- lib/watversion.ex
+++ lib/watversion.ex
@@ -3,15 +3,6 @@ defmodule Watversion do
   Documentation for Watversion.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Watversion.hello()
-      :world
-
-  """
   def hello do
     case Version.parse("1.1.0") do
       {:ok, whatever} -> whatever
